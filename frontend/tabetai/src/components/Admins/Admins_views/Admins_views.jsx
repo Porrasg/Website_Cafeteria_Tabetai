a8b01003cf36b1b080de5f5a74ca86f492b7745a
marchelino04 <marcelomaltes@gmail.com>
2023-12-13 13:40:56 -0600

    css
    Css style for the admin views

--- frontend/tabetai/src/components/Admins/Admins_views/Admins_views.jsx
+++ frontend/tabetai/src/components/Admins/Admins_views/Admins_views.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './Admins_view.css'
+import './Admins_view.css';
 
 const Admins_views = () => {
     const [admins, setAdmins] = useState([]);
@@ -7,18 +7,18 @@ const Admins_views = () => {
 
     useEffect(() => {
         const fetchAdmins = async () => {
-        try {
-            const response = await fetch('http://localhost:3001/api/v1/admins');
+            try {
+                const response = await fetch('http://localhost:3001/api/v1/admins');
 
-            if (!response.ok) {
-            throw new Error('Error al obtener los administradores');
+                if (!response.ok) {
+                    throw new Error('Error al obtener los administradores');
+                }
+
+                const data = await response.json();
+                setAdmins(data);
+            } catch (error) {
+                console.error('Error:', error);
             }
-
-            const data = await response.json();
-            setAdmins(data);
-        } catch (error) {
-            console.error('Error:', error);
-        }
         };
 
         fetchAdmins();
@@ -30,18 +30,18 @@ const Admins_views = () => {
 
     const handleDeleteAdmin = async (id) => {
         try {
-        const response = await fetch(`http://localhost:3001/api/v1/admins/${id}`, {
-            method: 'DELETE',
-        });
+            const response = await fetch(`http://localhost:3001/api/v1/admins/${id}`, {
+                method: 'DELETE',
+            });
 
-        if (!response.ok) {
-            throw new Error('Error al eliminar el administrador');
-        }
+            if (!response.ok) {
+                throw new Error('Error al eliminar el administrador');
+            }
 
-        // Actualizar la lista de administradores después de eliminar
-        setAdmins((prevAdmins) => prevAdmins.filter((admin) => admin.id !== id));
+            // Actualizar la lista de administradores después de eliminar
+            setAdmins((prevAdmins) => prevAdmins.filter((admin) => admin.id !== id));
         } catch (error) {
-        console.error('Error:', error);
+            console.error('Error:', error);
         }
     };
 
@@ -51,29 +51,29 @@ const Admins_views = () => {
     
     const handleUpdateAdmin = async (id) => {
         try {
-        const response = await fetch(`http://localhost:3001/api/v1/admins/${id}`, {
-            method: 'PATCH',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(editingAdmin),
-        });
+            const response = await fetch(`http://localhost:3001/api/v1/admins/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(editingAdmin),
+            });
 
-        if (!response.ok) {
-            throw new Error('Error al actualizar el administrador');
-        }
+            if (!response.ok) {
+                throw new Error('Error al actualizar el administrador');
+            }
 
-        // Actualizar la lista de clientes después de la actualización
-        setAdmins((prevAdmins) =>
-            prevAdmins.map((admin) =>
-            admin.id === id ? { ...admin, ...editingAdmin } : admin
-            )
-        );
+            // Actualizar la lista de clientes después de la actualización
+            setAdmins((prevAdmins) =>
+                prevAdmins.map((admin) =>
+                    admin.id === id ? { ...admin, ...editingAdmin } : admin
+                )
+            );
 
-        // Salir del modo de edición
-        setEditingAdmin(null);
+            // Salir del modo de edición
+            setEditingAdmin(null);
         } catch (error) {
-        console.error('Error:', error);
+            console.error('Error:', error);
         }
     };
 
@@ -82,78 +82,70 @@ const Admins_views = () => {
             <div>
                 <h2>Lista de Administradores</h2>
                 <div className='admin_container'>
-                    <ul>
-                        {admins.map((admin) => (
-                        <li className='each_item' key={admin.id}>
-                            {editingAdmin && editingAdmin.id === admin.id ? (
-                            // Formulario de edición
-                            <>
-                                <label htmlFor='name'>Nombre: </label>
-                                <input
-                                type='text'
-                                id='name'
-                                value={editingAdmin.name}
-                                onChange={(e) =>
-                                    setEditingAdmin({
-                                    ...editingAdmin,
-                                    name: e.target.value,
-                                    })
-                                }
-                                />
-
-                                <br />
-                                <br />
-
-                                {/* Agrega más campos según la estructura de tu API */}
-                                <label htmlFor='email'>Email: </label>
-                                <input
-                                type='text'
-                                id='email'
-                                value={editingAdmin.email}
-                                onChange={(e) =>
-                                    setEditingAdmin({
-                                    ...editingAdmin,
-                                    email: e.target.value,
-                                    })
-                                }
-                                />
-
-                                <br />
-                                <br />
-
-                                <label htmlFor='created_at'>Creado: </label>
-                                <input
-                                type='text'
-                                id='created_at'
-                                value={editingAdmin.created_at}
-                                onChange={(e) =>
-                                    setEditingAdmin({
-                                    ...editingAdmin,
-                                    created_at: e.target.value,
-                                    })
-                                }
-                                />
-
-                                <br />
-                                <br />
-
-                                <button onClick={() => handleUpdateAdmin(admin.id)}>
-                                Guardar
-                                </button>
-                                <button onClick={handleCancelEdit}>Cancelar</button>
-                            </>
-                            ) : (
-                            // Vista normal
-                            <>
-                                <p>Nombre: {admin.name}</p>
-                                <p>Email: {admin.email}</p>
-                                <p>Creado: {admin.created_at}</p>
-                                {/* Agrega más campos según la estructura de tu API */}
-                                <button onClick={() => handleEditClick(admin)}>Editar</button>
-                                <button onClick={() => handleDeleteAdmin(admin.id)}>Eliminar</button>
-                            </>
-                            )}
+                    <ul className='table'>
+                        <li className='table-header'>
+                            <span>Nombre</span>
+                            <span>Email</span>
+                            <span>Creado</span>
+                            <span>Acciones</span>
                         </li>
+                        {admins.map((admin) => (
+                            <li className='each_item' key={admin.id}>
+                                {editingAdmin && editingAdmin.id === admin.id ? (
+                                    // Formulario de edición
+                                    <>
+                                        <input
+                                            type='text'
+                                            value={editingAdmin.name}
+                                            onChange={(e) =>
+                                                setEditingAdmin({
+                                                    ...editingAdmin,
+                                                    name: e.target.value,
+                                                })
+                                            }
+                                        />
+                                        <input
+                                            type='text'
+                                            value={editingAdmin.email}
+                                            onChange={(e) =>
+                                                setEditingAdmin({
+                                                    ...editingAdmin,
+                                                    email: e.target.value,
+                                                })
+                                            }
+                                        />
+                                        <input
+                                            type='text'
+                                            value={editingAdmin.created_at}
+                                            onChange={(e) =>
+                                                setEditingAdmin({
+                                                    ...editingAdmin,
+                                                    created_at: e.target.value,
+                                                })
+                                            }
+                                        />
+                                        <button onClick={() => handleUpdateAdmin(admin.id)}>
+                                            Guardar
+                                        </button>
+                                        <button onClick={handleCancelEdit}>Cancelar</button>
+                                    </>
+                                ) : (
+                                    // Vista normal
+                                    <>
+                                        <span>{admin.name}</span>
+                                        <span>{admin.email}</span>
+                                        <span>{admin.created_at}</span>
+                                        <span>
+                                            <button onClick={() => handleEditClick(admin)}>
+                                                Editar
+                                            </button>
+                                            <button onClick={() => handleDeleteAdmin(admin.id)}>
+                                                Eliminar
+                                            </button>
+                                        </span>
+                                    </>
+                                )}
+                            </li>
                         ))}
                     </ul>
                 </div>
